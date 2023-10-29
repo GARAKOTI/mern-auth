4ffa343f488f9a604ee92e845a2e9fdf8fbaddf0
Garakoti <rahulsinghgarakoti730@gmail.com>
2023-10-30 01:34:43 +0530

    Connected to database

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,19 @@
+dotenv.config();
+import * as  dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
+    console.log("conected to Mongo db");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("server is working fine on 3000 port!");
+  console.log("server is working fine on 3000 port");
 });
